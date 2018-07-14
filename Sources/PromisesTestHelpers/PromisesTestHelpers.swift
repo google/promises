1e15edbd802c7569e682ab48dd8da1fac144d97f
Anthony Shoumikhin <anthony@shoumikh.in>
2018-07-14 15:59:00 -0700

    Tiny refinements: see commit descriptions (#60)
    * Apply extension-safe falg for framework targets only to avoid warnings when linking against libswiftXCTest.dylib for Swift tests.
    
    * Fix Error.userInfo type.

--- Sources/PromisesTestHelpers/PromisesTestHelpers.swift
+++ Sources/PromisesTestHelpers/PromisesTestHelpers.swift
@@ -43,7 +43,7 @@ public struct Test {
 public extension Error {
   var domain: String { return (self as NSError).domain }
   var code: Int { return (self as NSError).code }
-  var userInfo: [AnyHashable: Any] { return (self as NSError).userInfo }
+  var userInfo: [String: Any] { return (self as NSError).userInfo }
 }
 
 /// Compare two `Error?`.
